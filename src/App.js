7b5981cf3f0fe8ae5f4d148643a6b8ee8ae41178
WHSRobotics-MillanH <millan_hamilton@myabcusd.org>
2025-09-05 12:42:06 -0700

    asdfasdfasdfasdf

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='center'>
+        <a className='redirect' href="https://donate.stripe.com/bJe5kD33u9IW1hG1Dhc3m04">Donate</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
